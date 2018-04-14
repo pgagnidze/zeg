c43a133841f9f7a1df7b2f5b356c8389b2172a4f
Papuna Gagnidze <papungag@gmail.com>
2018-04-14 23:50:51 +0400

    Update init script and add gitignore

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,6 +2,10 @@
 
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum -y install puppet-agent
+sudo yum -y install git
+
+git clone https://github.com/papungag/zeg.git
+sudo mv zeg/manifests/* /etc/puppetlabs/code/environments/production/manifests/
 
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs-docker --version 1.1.0
