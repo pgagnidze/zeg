273163e43f442e763066256e91b0b6bcaa18f94d
Papuna Gagnidze <papungag@gmail.com>
2018-04-15 16:17:11 +0400

    better configs

--- data/add_host.py
+++ data/add_host.py
@@ -11,22 +11,25 @@ zapi.login('Admin', 'zabbix')
 print("Connected to Zabbix API Version %s" % zapi.api_version())
 
 host_name = 'WSJ'
-
-hostid = zapi.host.create({ 
-    "host": host_name,
-    "interfaces": [
-        {
-            "type":1,
-            "main":1,
-            "useip":1,
-            "ip":"127.0.0.1",
-            "dns":"",
-            "port":10050
-        }
-    ],
-    "groups": [
-        {
-            "groupid": "1"
-        }
-    ]})["hostids"][0]
-print("Added host with hostname {}".format(host_name))
+hosts = zapi.host.get(filter={"host": host_name}, selectInterfaces=["interfaceid"])
+if not hosts:
+    hostid = zapi.host.create({ 
+        "host": host_name,
+        "interfaces": [
+            {
+                "type":1,
+                "main":1,
+                "useip":1,
+                "ip":"127.0.0.1",
+                "dns":"",
+                "port":10050
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "1"
+            }
+        ]})["hostids"][0]
+    print("Added host with hostname {}".format(host_name))
+else:
+    print("Host with {} name already exists".format(host_name))
--- data/scrape.py
+++ data/scrape.py
@@ -1,11 +1,13 @@
 from lxml import html
 from lxml import cssselect
+import os
 import requests
 import json
 
 page = requests.get('http://markets.wsj.com/us')
 tree = html.fromstring(page.content)
 prices = tree.xpath('//div[@id="majorStockIndexes_moduleId"]/table/tbody/tr')
+cwd = os.getcwd()
 
 data = []
 for price in prices:
@@ -15,5 +17,5 @@ for price in prices:
         'last': price[1].text
     })
 
-with open('data.json', 'w') as outfile:
+with open(cwd + '/data.json', 'w') as outfile:
     json.dump(data, outfile)
