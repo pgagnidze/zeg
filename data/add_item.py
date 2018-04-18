c8e489bb50a589e832e8fdf0623553ea35d203a2
Papuna Gagnidze <papungag@gmail.com>
2018-04-18 20:51:10 +0400

    Update startup script time period

--- data/add_item.py
+++ data/add_item.py
@@ -18,28 +18,6 @@ json_dict = json.loads(data)
 
 hosts = zapi.host.get(filter={"host": host_name}, selectInterfaces=["interfaceid"])
 
-# Delete old items
-
-items = zapi.item.get({
-    "hostids": hosts[0]["hostid"],
-})
-
-itemids = []
-for i in range(-1, -9, -1):
-    itemids.append(items[i]["itemid"])
-
-if itemids[0] > '28250':
-    for itemid in itemids:
-        itemdel = zapi.item.delete(
-            itemid
-        )
-        print("Deleted item with id {}".format(itemid))
-        latest_item_id = itemid
-else:
-    print("Cannot delete templated items")
-
-# Add new items
-
 if hosts:
     host_id = hosts[0]["hostid"]
     print("Found host id {0}".format(host_id))
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,6 @@
 
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent git python-pip
-pip install pyzabbix lxml cssselect
 
 if [ -d "$HOME/zeg" ]; then
   cd $HOME/zeg && git reset --hard && git pull
@@ -15,7 +14,9 @@ mv $HOME/zeg/manifests/* /etc/puppetlabs/code/environments/production/manifests/
 /opt/puppetlabs/bin/puppet module install puppetlabs-docker --version 1.1.0
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/zabbix.pp
 
-2m
+3m
+
+pip install pyzabbix lxml cssselect
 
 mv $HOME/zeg/data/* $HOME/
 /usr/bin/python $HOME/add_host.py
