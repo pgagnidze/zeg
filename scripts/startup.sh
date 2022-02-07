8e2963a23bd4aa790847b315efd1b9812e0e19a6
Papuna Gagnidze <papungag@gmail.com>
2022-02-07 17:22:38 +0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -11,13 +11,6 @@ else
 fi
 
 mv $HOME/zeg/manifests/* /etc/puppetlabs/code/environments/production/manifests/
-mv $HOME/zeg/data/* $HOME/
-crontab -l | { cat; echo "* * * * * /usr/bin/python $HOME/scrape.py"; } | crontab -
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 /opt/puppetlabs/bin/puppet module install puppetlabs-docker --version 1.1.0
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/zabbix.pp
-
-sleep 2m
-
-/usr/bin/python $HOME/add_host.py
-/usr/bin/python $HOME/add_item.py
